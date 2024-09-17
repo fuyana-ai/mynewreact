078708a5fec871d9a1528002c9bb23704b66fec5
fuyana-ai <culanimsekiwa@yahoo.com>
2024-09-17 10:24:24 +0200

    created app.js file

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react';
 
-function App() {
+// const Person = (props) => {
+//   return (
+//     <>
+//       <h1> Name: {props.name}</h1>
+//       <h2> Last Name: {props.lastName}</h2>
+//       <h2> Age: {props.age}</h2>
+//     </>
+//   )
+// }
+
+const App = () => {
+  const [counter, setCounter] = useState(0);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={() => setCounter((prevCount) => prevCount - 1)}>-</button>
+<h1>{counter}</h1>
+<button> </button>
+
+        {/* <header className="App-header">
+        <h1> Hello World</h1>
+        <h2> My name is {myname}</h2>
+        <Person name='Lindsay'
+          Lastname='Fuyana'
+          Age={30} />
+
+        <Person name={'Culani'}
+          LastName='Fuy'
+          Age={25} />
+
+        <Person name={'Michell'}
+          LastName='Shonge'
+          Age={26} />
+
+        <Person name={'Hilda'}
+          LastName='MaNyathi'
+          Age={10} />
+
+        <Person name={'Anaya'}
+          LastName='Anashe'
+          Age={5} />
+
+
+      </header> */}
     </div>
   );
 }
