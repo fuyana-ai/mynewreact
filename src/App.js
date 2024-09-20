7b6537461967c94406d4c0f4e087b9093fa9403e
fuyana-ai <culanimsekiwa@yahoo.com>
2024-09-20 10:26:46 +0200

    Created movie search app

--- src/App.js
+++ src/App.js
@@ -1,52 +1,56 @@
-import './App.css';
-import { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 
-// const Person = (props) => {
-//   return (
-//     <>
-//       <h1> Name: {props.name}</h1>
-//       <h2> Last Name: {props.lastName}</h2>
-//       <h2> Age: {props.age}</h2>
-//     </>
-//   )
-// }
+import MovieCard from "./MovieCard";
+import SearchIcon from "./search.svg";
+import "./App.css";
+
+const API_URL = "http://www.omdbapi.com?apikey=b6003d8a";
 
 const App = () => {
-  const [counter, setCounter] = useState(0);
+  const [searchTerm, setSearchTerm] = useState("");
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    searchMovies("Batman");
+  }, []);
+
+  const searchMovies = async (title) => {
+    const response = await fetch(`${API_URL}&s=${title}`);
+    const data = await response.json();
+
+    setMovies(data.Search);
+  };
 
   return (
-    <div className="App">
-      <button onClick={() => setCounter((prevCount) => prevCount - 1)}>-</button>
-<h1>{counter}</h1>
-<button> </button>
+    <div className="app">
+      <h1>MovieLand</h1>
 
-        {/* <header className="App-header">
-        <h1> Hello World</h1>
-        <h2> My name is {myname}</h2>
-        <Person name='Lindsay'
-          Lastname='Fuyana'
-          Age={30} />
+      <div className="search">
+        <input
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+          placeholder="Search for movies"
+        />
+        <img
+          src={SearchIcon}
+          alt="search"
+          onClick={() => searchMovies(searchTerm)}
+        />
+      </div>
 
-        <Person name={'Culani'}
-          LastName='Fuy'
-          Age={25} />
-
-        <Person name={'Michell'}
-          LastName='Shonge'
-          Age={26} />
-
-        <Person name={'Hilda'}
-          LastName='MaNyathi'
-          Age={10} />
-
-        <Person name={'Anaya'}
-          LastName='Anashe'
-          Age={5} />
-
-
-      </header> */}
+      {movies?.length > 0 ? (
+        <div className="container">
+          {movies.map((movie) => (
+            <MovieCard movie={movie} />
+          ))}
+        </div>
+      ) : (
+        <div className="empty">
+          <h2>No movies found</h2>
+        </div>
+      )}
     </div>
   );
-}
+};
 
 export default App;
